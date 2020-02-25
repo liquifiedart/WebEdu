59b3922b9ba51d511985dc82365d10b5b554fa08
liquifiedart <mydeveloperportal@gmail.com>
2020-02-25 07:56:25 -0500

    added suggest page and form

---
+++ Recipe Site/scripts/suggest.js
@@ -0,0 +1,8 @@
+
+function submitForm(){
+                 
+    console.log("read: " + document.getElementById("frname").value);
+    console.log("read: " + document.getElementById("fingredients").value);
+    console.log("read: " + document.getElementById("fname").value);
+    
+};
